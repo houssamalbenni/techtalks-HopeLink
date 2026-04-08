89888633d177dff679d38a1abcf781602459e58e
abed-bortol <www.abdelwahab.bortol.4@gmail.com>
2026-04-09 00:09:28 +0300

    feat: add notification schema and DB connection setup

--- server/src/constant/enum.js
+++ server/src/constant/enum.js
@@ -6,5 +6,18 @@ module.exports = {
   FamilyRelations: ['brother', 'daughter', 'sister', 'father', 'mother', 'spouse', 'son', 'others'],
   RequestStatus: ['pending', 'approved'],
   PriorityLevels: ['urgent', 'high', 'standard', 'low'],
-  MissingStatus: ['matched', 'inProgress']
+  MissingStatus: ['matched', 'inProgress'],
+  NotificationPriority: ['critical', 'important', 'normal'],
+  NotificationTypes: [
+      'emergency_alert',
+      'aid_request_update',
+      'shelter_update',
+      'food_update',
+      'medicine_update',
+      'appointment_reminder',
+      'ngo_announcement',
+      'missing_person_update',
+      'donation_update',
+      'system',
+    ]
 };
