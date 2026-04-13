1073055f051f436918f9de871d49c2b2e2d35629
Hanan07 <hanan.o.alhaddad@gmail.com>
2026-04-13 13:48:56 +0300

    add admin services

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+
 const { loginRefugeeController, registerRefugeeController }= require('../controller/auth.controller.js');
 const { loginRules, registerRefugeeRules } =require('../middleware/auth.validation.js');
 const userController = require('../controller/auth.controller.js');
@@ -7,6 +8,23 @@ const userController = require('../controller/auth.controller.js');
 router.post('/register', registerRefugeeRules, userController.registerRefugeeController);
 
 // ─── Login Route ──────────────────────────────────────────────────────────────
-// router.post('/login', loginRules, loginRefugeeController);
+ router.post('/login', loginRules, loginRefugeeController);
 
+
+
+// ─── Service Route (hospitals & shelters) ──────────────────────────────────────────────────────────────
+
+
+
+
+// Create ANY service (hospital, shelter, etc.)
+const AdminController = require("../controller/admin.controller");
+
+// Get nearby services (geo query)
+router.get("/services/nearby", AdminController.getNearby);
+// Create service (hospital / shelter)
+router.post("/services", AdminController.createService);
+
+// Get all services
+router.get("/services", AdminController.getServices);
 module.exports = router;
